f540f82c94c042edb285f57395b0b0f1b50d16c7
emmanuelmargueritte <emmanuel.margueritte@gmail.com>
2025-12-12 19:27:51 +1100

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const app=express();
 app.use(express.json());
 app.use(express.static('.'));
 app.post('/ajouter',(req,res)=>{
-    let d=JSON.parse(fs.readFileSync('data/produits.json'));
+    let d=JSON.parse(fs.readFileSync('/produits.json'));
     d.push(req.body);
-    fs.writeFileSync('data/produits.json',JSON.stringify(d,null,2));
+    fs.writeFileSync('/produits.json',JSON.stringify(d,null,2));
     res.sendStatus(200);
 });
 app.post('/supprimer',(req,res)=>{
-    let d=JSON.parse(fs.readFileSync('data/produits.json'));
+    let d=JSON.parse(fs.readFileSync('/produits.json'));
     d.splice(req.body.index,1);
-    fs.writeFileSync('data/produits.json',JSON.stringify(d,null,2));
+    fs.writeFileSync('/produits.json',JSON.stringify(d,null,2));
     res.sendStatus(200);
 });
 app.listen(3000,()=>console.log('OK 3000'));
